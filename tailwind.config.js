bd0d7c76c2b73172a13b428a2b9483c1dc4a98f4
Meheraj1000 <karim23105101399@diu.edu.bd>
2024-08-18 11:29:45 +0100

    new add footer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,12 @@ module.exports = {
     },
     plugins: [],
   }
+
+
+//   <ul class="space-y-2">
+//   <li><a class="hover:text-orange-500" href="#">Email address</a></li>
+//   <hr>
+//    <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+//    <a href=""><i class="fa-brands fa-twitter"></i></a>
+//    <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+//  </ul> 
